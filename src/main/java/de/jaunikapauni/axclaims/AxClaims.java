e8a461036529590cbb48f43c5902857545743feb
JauniKapauni <riches-goods.54@icloud.com>
2026-05-14 15:56:11 +0200

    init: initialize project

---
+++ src/main/java/de/jaunikapauni/axclaims/AxClaims.java
@@ -0,0 +1,16 @@
+package de.jaunikapauni.axclaims;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class AxClaims extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
