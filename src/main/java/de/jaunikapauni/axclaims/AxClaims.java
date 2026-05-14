4206807eb645bc8eaa81786566e6e3709ce1a7d3
JauniKapauni <riches-goods.54@icloud.com>
2026-05-14 16:26:28 +0200

    feat: add PVPListener

--- src/main/java/de/jaunikapauni/axclaims/AxClaims.java
+++ src/main/java/de/jaunikapauni/axclaims/AxClaims.java
@@ -3,6 +3,7 @@ package de.jaunikapauni.axclaims;
 import de.jaunikapauni.axclaims.command.ClaimCommand;
 import de.jaunikapauni.axclaims.listener.BlockBreakListener;
 import de.jaunikapauni.axclaims.listener.BlockPlaceListener;
+import de.jaunikapauni.axclaims.listener.PVPListener;
 import de.jaunikapauni.axclaims.manager.Claim;
 import org.bukkit.plugin.java.JavaPlugin;
 
@@ -21,6 +22,7 @@ public final class AxClaims extends JavaPlugin {
         getCommand("claim").setExecutor(new ClaimCommand(this));
         getServer().getPluginManager().registerEvents(new BlockBreakListener(this), this);
         getServer().getPluginManager().registerEvents(new BlockPlaceListener(this), this);
+        getServer().getPluginManager().registerEvents(new PVPListener(this), this);
     }
 
     @Override
---
+++ src/main/java/de/jaunikapauni/axclaims/listener/PVPListener.java
@@ -0,0 +1,29 @@
+package de.jaunikapauni.axclaims.listener;
+
+import de.jaunikapauni.axclaims.AxClaims;
+import de.jaunikapauni.axclaims.manager.Claim;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.entity.EntityDamageByEntityEvent;
+
+public class PVPListener implements Listener {
+    AxClaims reference;
+    public PVPListener(AxClaims reference){
+        this.reference = reference;
+    }
+
+    @EventHandler
+    public void onPVP(EntityDamageByEntityEvent e){
+        Player attacker = (Player) e.getDamager();
+        Player victim = (Player) e.getEntity();
+
+        for(Claim claim : reference.getAllClaims()){
+            if(claim.isInside(victim.getLocation())){
+                e.setCancelled(true);
+                attacker.sendMessage("PVP is deactivated for this region!");
+                return;
+            }
+        }
+    }
+}
